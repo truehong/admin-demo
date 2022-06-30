a5f035889ec8c09eb219a52ce9ecf1d1ef35c47c
truehong <hhhhhongse@gmail.com>
2022-07-01 00:52:24 +0900

    add controller, view

---
+++ src/main/java/com/admin/account/SignUpForm.java
@@ -0,0 +1,12 @@
+package com.admin.account;
+
+import lombok.Data;
+
+@Data
+public class SignUpForm {
+    private String nickname;
+
+    private String email;
+
+    private String password;
+}
---
+++ src/test/java/com/admin/account/AccountControllerTest.java
@@ -0,0 +1,35 @@
+package com.admin.account;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AccountControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("회원 가입을 하면 보여지는 테스트")
+    void signUpForm() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/sign-up"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("account/sign-up"))
+                .andExpect(model().attributeExists("signUpForm"));
+
+    }
+
+}
