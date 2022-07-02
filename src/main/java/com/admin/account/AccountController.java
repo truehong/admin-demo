b77b0583c41b20659eb2bd9019bd521d22afd625
truehong <hhhhhongse@gmail.com>
2022-07-02 18:12:32 +0900

    add validation, email Sender

--- src/main/java/com/admin/account/AccountController.java
+++ src/main/java/com/admin/account/AccountController.java
@@ -1,15 +1,69 @@
 package com.admin.account;
 
+import com.admin.domain.Account;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
+@RequiredArgsConstructor
 public class AccountController {
+
+    private final SignUpFormValidator signUpFormValidator;
+    private final AccountRepository accountRepository;
+    private final MailSender javaMailSender;
+
+    @InitBinder("signUpForm")
+    public void initBinder(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(signUpFormValidator);
+    }
+
     @GetMapping("/sign-up")
-    public String signUpForm(Model model){
+    public String signUpForm(Model model) {
         model.addAttribute("signUpForm", new SignUpForm());
         return "account/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors) {
+        if (errors.hasErrors()) {
+            return "account/sign-up";
+        }
+
+        Account account = Account
+                .builder()
+                .email(signUpForm.getEmail())
+                .nickname(signUpForm.getNickname())
+                .password(signUpForm.getPassword()) // TODO encoding 해야함
+                .build();
+
+        Account newAccount = accountRepository.save(account);
+
+        newAccount.generateEmailCheckToken();
+
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setSubject("어드민, 회원 가입 인증");
+        mailMessage.setTo(newAccount.getEmail());
+        mailMessage.setText("/check-email-token?token="
+                + newAccount.getEmailCheckToken()
+                +"&email="
+                + newAccount.getEmail());
+
+        javaMailSender.send(mailMessage);
+        // TODO 회원 가입 처리
+        return "redirect:/";
 
     }
 }
